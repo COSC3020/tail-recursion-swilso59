b59fd666e825866ab1890ef0c6bb1d81d312c15c
Sean W <104464288+swilso59@users.noreply.github.com>
2024-05-07 19:49:01 -0600

    Create code.js

---
+++ code.js
@@ -0,0 +1,9 @@
+function fib(n, a = 0, b = 1, fibArr = []) {
+    if (n === 0) {
+        fibArr.push(a);
+        return fibArr;
+    } else {
+        fibArr.push(a);
+        return fib(n - 1, b, a + b, fibArr);
+    }
+}
